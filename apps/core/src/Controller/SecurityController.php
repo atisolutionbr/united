a79adff9fa0924c06feab10d40ebf0c3bca18b07
Jonatha Soares <jonatha.soares@atisolution.com.br>
2026-09-04 10:36:23 -0300

    fix: keep login redirects on local kflow route

--- apps/core/src/Controller/SecurityController.php
+++ apps/core/src/Controller/SecurityController.php
@@ -50,7 +50,7 @@ final class SecurityController extends AbstractController
         // page, after a successful login you are redirected to a page in the previous
         // locale. This code regenerates the referrer URL whenever the login page is
         // browsed, to ensure that its locale is always the current one.
-        $this->saveTargetPath($request->getSession(), 'main', $this->generateUrl('kflow_dashboard'));
+        $this->saveTargetPath($request->getSession(), 'main', '/kflow');
 
         return $this->render('security/login.html.twig', [
             // last username entered by the user (if any)
